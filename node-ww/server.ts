2df471925589f547b75c314a372472908ba62fe4
Michael Arnold Owens <anpganpg@gmail.com>
2024-06-28 01:16:00 -0700

    sorting and reducing object size of repos

--- node-ww/server.ts
+++ node-ww/server.ts
@@ -7,23 +7,6 @@ const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
-// const setHeaders = function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("X-GitHub-Api-Version", "2022-11-28");
-//   res.setHeader("Accept", "application/vnd.github+json");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type",
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next(); // invokes next middlewear in the stack
-// };
-// app.use(setHeaders);
-
 app.use(express.json());
 app.use(cors());
 
@@ -31,11 +14,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Github NodeJS API app!");
 });
 
-app.get("/org_repos", (req: Request, res: Response) => {
-  const { org } = req.query;
-  const options = {
+const generateOptions = (path: string) => {
+  return {
     hostname: "api.github.com",
-    path: "/orgs/github/repos",
+    path,
     method: "GET",
     headers: {
       Accept: "application/vnd.github+json",
@@ -44,20 +26,57 @@ app.get("/org_repos", (req: Request, res: Response) => {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   };
+};
+
+app.get("/org_repos", (req: Request, res: Response) => {
+  const { org } = req.query;
+  const options = generateOptions(`/orgs/${org}/repos`);
 
   https
     .get(options, (response) => {
       let data = "";
 
-      // A chunk of data has been received.
       response.on("data", (chunk) => {
         data += chunk;
       });
 
-      // The whole response has been received.
       response.on("end", () => {
         try {
-          const jsonData = JSON.parse(data);
+          const jsonData = JSON.parse(data)
+            .sort((a: { forks_count: number }, b: { forks_count: number }) => {
+              const { forks_count: forks_count_a } = a;
+              const { forks_count: forks_count_b } = b;
+              if (forks_count_a > forks_count_b) {
+                return -1;
+              } else if (forks_count_b > forks_count_a) {
+                return 1;
+              }
+              return 0;
+            })
+            // TODO: type this or just use octokit.js
+            .map((repo: any) => {
+              const {
+                name,
+                stargazers_count,
+                watchers_count,
+                forks_count, // forks seems to be a duplicated field
+                open_issues_count,
+                language,
+                license,
+                owner,
+              } = repo;
+              return {
+                name,
+                stargazers_count,
+                watchers_count,
+                forks_count,
+                open_issues_count,
+                language,
+                license,
+                owner: owner.login,
+              };
+            });
+          console.log(jsonData);
           res.json(jsonData);
         } catch (err) {
           console.error("Error parsing JSON:", err);
@@ -69,18 +88,6 @@ app.get("/org_repos", (req: Request, res: Response) => {
       console.error("Error: ", err.message);
       res.status(500).send("Error fetching data from GitHub");
     });
-  // https.get(options, (apiResponse: IncomingMessage) => {
-  //   let data = "";
-  //   apiResponse.on("data", (chunk) => {
-  //     data += chunk;
-  //   });
-
-  //   apiResponse.on("end", () => {
-  //     // const jsonData = JSON.parse(data);
-  //     console.log("data:", data);
-  //     res.json(data);
-  //   });
-  // });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
